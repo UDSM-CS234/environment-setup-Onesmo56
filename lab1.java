192da0371a282a860e9b6726a2d6d6fa777586c2
onesmomboya <onesmomboya095@gmail.com>
2022-05-19 18:21:53 +0300

    Lab 1 Assignment

---
+++ lab1.java
@@ -0,0 +1,12 @@
+public class lab1 {
+    public static void main(String[] args) {
+        double a= -9.81;
+        double Vi=0;
+        double Xi=0;
+        double Xt;
+        int t=10;
+        Xt= 0.5*a*t*t + Vi*t + Xi;
+        System.out.println("The value of Xi is: " + Xt + "m");
+    }
+
+}
